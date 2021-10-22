47d5db3eec6ad2d3d5e8cf464a2ab1c3689e2f01
Charles Pierse <charlespierse@gmail.com>
2021-10-22 12:27:47 +0100

    Alter dump exlcusion rules

--- scrub_and_shrink.py
+++ scrub_and_shrink.py
@@ -30,13 +30,10 @@ def scrub_chunks():
             for m in movies.keys():
                 dat = movies[m]
                 if ( not dat["adult"] and
-                    dat["vote_count"] > 5 and
-                    dat["budget"] > 1 and
-                    dat["original_language"] == "en" and
-                    dat["poster_path"] is not None and
-                    dat["runtime"] is not None and
-                    dat["runtime"] > 59 and
-                    dat["release_date"] 
+                    dat["vote_count"] > 30 
+                    # dat["budget"] > 1 and
+                    # # dat["original_language"] == "en" and
+                    # # dat["poster_path"] is not None and
                     ):
                     k = dat["id"]
                     keep.update({k : dat})
